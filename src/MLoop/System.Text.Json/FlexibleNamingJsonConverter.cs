a286e3680a6752fe5624c2397a65048d36efd3c3
uj <caveman@iyulab.com>
2024-07-08 18:24:02 +0900

    init

--- src/MLoop/System.Text.Json/FlexibleNamingJsonConverter.cs
+++ src/MLoop/System.Text.Json/FlexibleNamingJsonConverter.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 using System.Reflection;
-using System.Linq;
 
 namespace System.Text.Json
 {
